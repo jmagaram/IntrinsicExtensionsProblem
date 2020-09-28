4187391465ab80cde10555c1d860ce4f78eafea0
Justin Magaram <justin@magaram.com>
2020-09-27 21:13:22 -0700

    Add project files.

---
+++ ClassLibrary1/Contacts.fs
@@ -0,0 +1,26 @@
+﻿namespace Contacts
+
+module AddressBookTypes =
+
+    type Person = { FirstName: string; LastName: string }
+
+module AddressBookFunctions =
+
+    open AddressBookTypes
+
+    let fullName (p: Person) = p.FirstName + " " + p.LastName
+
+    type Person with
+        member public me.FullName = me |> fullName
+
+    // This works as expected
+    let getFullName1 (p: Person) = p.FullName
+
+module SomeOtherModule =
+
+    open AddressBookTypes
+    open AddressBookFunctions
+
+    // This works too but only if I also "open AddressBookFunctions"
+    // Seems to behave like an extension method
+    let getFullName2 (p: Person) = p.FullName
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp17 {
+    class Program {
+        static void Main(string[] args) {
+            var person = new Contacts.AddressBookTypes.Person("Bob", "Smith");
+
+            // This line will not compile. Can not get this any visibility on the "FullName" member from C#
+            //var s = person.FullName;
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
